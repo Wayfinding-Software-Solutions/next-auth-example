ad62baa8df68ab733ff78b6971d616d69acaa706
Brett Clanton <brettclanton001@protonmail.com>
2024-06-05 20:08:41 +0000

    more fat cutting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,43 @@
-import "./globals.css"
-import type { Metadata } from "next"
 import type { ReactNode } from "react"
 import { auth } from "auth"
-import { SignIn, SignOut } from "@/components/auth-components"
+import { signIn, signOut } from "auth"
 
-export const metadata: Metadata = {
-  title: "NextAuth.js Example",
-  description:
-    "This is an example site to demonstrate how to use NextAuth.js for authentication",
-}
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const session = await auth()
-  if (!session?.user) {
-    return (
-      <>
-        <SignIn />
-        <main>{children}</main>
-      </>
-    )
-  }
+  
   return (
-    <>
-      <div className="flex gap-2 items-center">
-        <div className="flex flex-col space-y-1">
-          <p className="text-sm font-medium leading-none">
-            {session.user.name}
-          </p>
-          <p className="text-xs leading-none text-muted-foreground">
-            {session.user.email}
-          </p>
-        </div>
-        <SignOut />
-      </div>
-      <main>{children}</main>
-    </>
+    <html lang="en">
+      <body>
+        { session?.user ? (
+          <div>
+            <p>
+              {session.user.name}
+            </p>
+            <p>
+              {session.user.email}
+            </p>
+            <form
+              action={async () => {
+                'use server'
+                await signOut()
+              }}
+            >
+              <button>Sign Out</button>
+            </form>
+          </div>
+        ) : (
+          <form
+            action={async () => {
+              'use server'
+              await signIn()
+            }}
+          >
+            <button>Sign In</button>
+          </form>
+        ) }
+        <main>{children}</main>
+      </body>
+    </html>
   )
 }
